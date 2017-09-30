4e73c86f85ddfbadf5fdb921e6ba5476146e7c7d
tawany <tawanyvilar@gmail.com>
2017-09-30 18:31:08 -0300

    Add controle de estoque

--- src/Form.java
+++ src/Form.java
@@ -2,10 +2,10 @@ public class Form {
 
     public static void main(String[] args){
 
-        int a = 0, b = 100, x = 0;
+        int ab = 0, b = 100, x = 0;
 
         for(; x < b; x++){
-            a = a + x;
+            ab = ab + x;
         }
     }
 
